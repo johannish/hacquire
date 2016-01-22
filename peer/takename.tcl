de406e36a41cdbac959a4f4cd146b584dfc1d7eb
LegitMiller <jordan.kay@gmail.com>
2016-01-21 20:50:47 -0700

    experimented with comm, included rules for reference

--- peer/takename.tcl
+++ peer/takename.tcl
@@ -1,13 +1,17 @@
 #! /bin/env tclsh
 package require comm
+
+
 proc main {argv0 argv} {
-	lassign [chan pipe] pr pw 
-	exec [info nameofexecutable]  [file dirname [info script]]/myname.tcl >@$pw &
+	set talkto "test.tcl"
+	set callto "title"
+	lassign [chan pipe] pr pw
+	exec [info nameofexecutable]  [file dirname [info script]]/$talkto >@$pw &
 	close $pw
 	set data [gets $pr]
 	set controlport [dict get $data port]
 	puts $data
 	::comm::comm send $controlport puts a
-	puts [::comm::comm send $controlport tile]
+	puts [::comm::comm send $controlport $callto]
 }
 main $argv0 $argv
